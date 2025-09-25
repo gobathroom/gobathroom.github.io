ef3d877583684c077e052fae7872bb30dbe9b73b
gobathroom <gobathroomgo@gmail.com>
2025-09-25 07:26:13 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+function armTapReset(a) {
+  a.addEventListener('click', function () {
+    this.classList.add('tapped');
+    setTimeout(() => this.blur(), 50);
+    setTimeout(() => this.classList.remove('tapped'), 1500);
+  }, { passive: true });
+
+  a.addEventListener('touchstart', function () {
+    this.classList.add('tapped');
+    setTimeout(() => this.blur(), 50);
+    setTimeout(() => this.classList.remove('tapped'), 1500);
+  }, { passive: true });
+}
+
+// Llama la función en ambos lados del botón doble
+document.querySelectorAll('.dual-button .btn-left')
+  .forEach(a => armTapReset(a));
+document.querySelectorAll('.dual-button .btn-right')
+  .forEach(a => armTapReset(a));
 
