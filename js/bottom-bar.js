533062f940f038425e884200f235e9056bc25075
gobathroom <gobathroomgo@gmail.com>
2025-11-15 14:43:10 -0500

    Update bottom-bar.js

--- js/bottom-bar.js
+++ js/bottom-bar.js
@@ -1,24 +1,9 @@
 /* =========================================================
-   1) bottom bar actions  (6/-)
+   1) bottom bar actions (solo estado activo) (6/-)
    ========================================================= */
 
+// nav inferior
 const bottomBar = document.getElementById("bottomBar");
-let lastScrollY = window.scrollY;
-
-window.addEventListener("scroll", () => {
-  const current = window.scrollY;
-
-  // bajando
-  if (current > lastScrollY + 5) {
-    bottomBar?.classList.add("hidden");
-  }
-  // subiendo
-  else if (current < lastScrollY - 5) {
-    bottomBar?.classList.remove("hidden");
-  }
-
-  lastScrollY = current;
-});
 
 // opcional: marcar activo cuando hacen click
 const bottomLinks = document.querySelectorAll(".bottom-bar .bottom-item");
