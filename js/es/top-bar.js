e5696049d957713183ab923759df72075d905ece
gobathroom <gobathroomgo@gmail.com>
2025-11-18 22:09:52 -0500

    Update top-bar.js

--- js/es/top-bar.js
+++ js/es/top-bar.js
@@ -1,1 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const brand = document.getElementById('brandLink');
 
+  if (brand) {
+    const path = window.location.pathname;
+
+    // Detecta idioma automáticamente
+    if (path.startsWith("/es")) {
+      brand.href = "/es";
+    } else {
+      brand.href = "/";
+    }
+  }
+});
